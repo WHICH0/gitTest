361e2a8f7fda3594cc006f757976805505747541
YXY <1608574660@qq.com>
2021-10-11 15:09:07 +0800

    push commit

--- src/main/java/com/git_/GitTest01.java
+++ src/main/java/com/git_/GitTest01.java
@@ -8,6 +8,7 @@ public class GitTest01 {
        System.out.println("hello4");
        System.out.println("master");
        System.out.println("hot-fix");
+       System.out.println("push");
 
    }
 }
